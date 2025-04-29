07d447aba254d6610a83814334307d6ac722c58d
Charles Villalpando <charlesvill@g.ucla.edu>
2025-04-28 21:37:13 -0700

    Blog comment routing & controllers

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -7,6 +7,81 @@ postRouter.get("/", (req, res, next) => {
   res.send("GET request for posts!");
 });
 
+postRouter.get("/:postid/comments", async (req, res) => {
+  // see all comments
+  const postId = req.params.postid;
+  try {
+    const response = await prisma.post.findUnique({
+      where: {
+        id: Number(postId),
+      },
+      include: {
+        comment: true,
+      }
+    });
+
+    res.json(response);
+  } catch (err) {
+    return next(new InternalServerError(err.message));
+  }
+  // res.send("See all comments from the " + postId + "post id");
+});
+
+postRouter.post("/:postid/comments/:userid", async (req, res) => {
+  // post a comment on a blog post
+  const postId = req.params.postid;
+  const userId = req.params.userid;
+
+  const {content} = req.body;
+
+  try {
+    const response = await prisma.comment.create({
+      data: {
+        content,
+        post: {
+          connect:{
+            id: Number(postId),
+          },
+        },
+        author: {
+          connect: {
+            id: Number(userId),
+          },
+        },
+      },
+      include: {
+        author: true,
+      },
+    });
+
+    res.json(response);
+} catch (err) {
+    return next(new InternalServerError(err.message));
+  }
+  // res.send("post a comment on the " + postId + "post id");
+});
+
+postRouter.delete(("/:postid/comments/:commentid"), async(req, res) => {
+  // delete a comment on a blog post
+  
+  const postId = req.params.postid;
+  const commentId = req.params.commentid;
+
+  try {
+    const response = await prisma.comment.delete({
+      where: {
+        id: Number(commentId),
+      },
+    });
+
+    res.json(response,  "there has been a deletion occur");
+  } catch (err) {
+    return next(new InternalServerError(err.message));
+  }
+
+  res.send("delete a comment on the " + postId + "post id and " + commentId + "comment id");
+});
+
 // get all posts by user
 postRouter.get("/:userid", async(req, res, next) => {
   const userId = req.params.userid;
@@ -65,7 +140,7 @@ postRouter.put("/:postid", async(req, res, next) => {
     title,
     content,
   } = req.body;
-  const postId = req.params.id;
+  const postId = req.params.postid;
 
   try {
     const response = await prisma.post.update({
@@ -83,5 +158,22 @@ postRouter.put("/:postid", async(req, res, next) => {
     return next(new InternalServerError(err.message));
   }
 });
+
+postRouter.delete("/:postid", async(req, res, next) => {
+  const postId = req.params.postid;
+
+  try {
+    const response = await prisma.post.delete({
+      where: {
+        id: Number(postId),
+      },
+    });
+
+    res.json(response);
+  } catch (err) {
+    return next(new InternalServerError(err.message));
+  }
+});
+
 
 module.exports = postRouter;
