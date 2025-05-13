0df26ea24e5b30937f31e93d322985d9d73b18de
Charles Villalpando <charlesvill@g.ucla.edu>
2025-05-12 22:38:30 -0700

    Skeleton Log in Component for testing

--- client/src/publisher/login.jsx
+++ client/src/publisher/login.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react"
+
+
+
 const TestLogin = () => {
+  const [data, setData] = useState({ username: "", password: "" });
+
+  function handleUpdate(e) {
+    // on every input change, the component needs to re-render to show updated user input
+
+    const fieldName = e.target.id;
+    const value = e.target.value;
+
+    setData({ ...data, [fieldName]: value });
+  }
+
+  function handleSubmit(e) {
+    // handle the submission. and fetching stuff from server?
+    e.preventDefault();
+    console.log(data);
+  }
+
   return (
+
+
     <div>
       this is the log in screen
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">User name</label>
+        <input type="text" id="username" onChange={handleUpdate} value={data.username} />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" onChange={handleUpdate} value={data.password} />
+        <button type="submit"></button>
+      </form>
     </div>
   )
 };
