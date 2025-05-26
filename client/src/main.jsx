1fbfa86d32aa9c715fcedc34b7097d58eaa1a013
Charles Villalpando <charlesvill@g.ucla.edu>
2025-05-26 11:36:01 -0700

    Refactored main and app component flow

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,27 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import TestAdmin from './publisher/testadmin.jsx'
-import TestLogin from './publisher/login.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/admin.blog",
-    element: <TestAdmin />,
-    children: [
-      { path: "login", element: <TestLogin />},
-    ],
-  },
-]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
+
+// main -> app w/ router with defined paths -> client homepage as index
