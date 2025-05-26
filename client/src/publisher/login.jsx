63c41c946070908ee0b7d4cec43b60282b0f2c5a
Charles Villalpando <charlesvill@g.ucla.edu>
2025-05-26 10:09:23 -0700

    Successful log in w/ REST api

--- client/src/publisher/login.jsx
+++ client/src/publisher/login.jsx
@@ -1,27 +1,66 @@
-import { useState } from "react"
-
+import { useState, useEffect } from "react"
 
 
 const TestLogin = () => {
   const [data, setData] = useState({ username: "", password: "" });
+  const [loading, setLoading] = useState(null);
+  const [result, setResult] = useState("");
 
   function handleUpdate(e) {
     // on every input change, the component needs to re-render to show updated user input
-
     const fieldName = e.target.id;
     const value = e.target.value;
 
     setData({ ...data, [fieldName]: value });
   }
 
-  function handleSubmit(e) {
-    // handle the submission. and fetching stuff from server?
+  async function handleSubmit(e) {
     e.preventDefault();
-    console.log(data);
+    const url = import.meta.env.VITE_LOCAL_LOGIN;
+    // state storing result of fetch
+
+    console.log(url);
+    setLoading(true);
+
+    // loading state along with loadig spinner
+    try {
+      const response = await fetch(
+        url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data),
+        mode: 'cors'
+      }).then(async (response) => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        } else {
+          const json = await response.json();
+          // update to data state when finished. 
+          setResult(json);
+          setLoading(false);
+        }
+      });
+
+    } catch (error) {
+      console.error(error);
+    }
+    // navigates to log in screen?
+  }
+
+  function LoggedIn({ result }) {
+    return (
+      <>
+        <p>hello {result.user.first_name}</p>
+        <div>
+          token: {result.token}
+        </div>
+      </>
+    )
   }
 
   return (
-
 
     <div>
       this is the log in screen
@@ -32,6 +71,7 @@ const TestLogin = () => {
         <input type="password" id="password" onChange={handleUpdate} value={data.password} />
         <button type="submit"></button>
       </form>
+      {loading ? <p>Loading</p> : (result && result.user.first_name) ? <LoggedIn result={result} /> : null}
     </div>
   )
 };
