c8f4e53d837d9186fbd9614cba2cca969a353000
Charles Villalpando <charlesvill@g.ucla.edu>
2025-05-28 17:42:02 -0700

    Local Storage & react context state management

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
 import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { useState, createContext } from 'react'
 import TestAdmin from './publisher/testadmin.jsx'
 import TestLogin from './publisher/login.jsx'
 import HomePage from './endUser/homepage.jsx'
+
+// local storage for token pull & push 
+// state for token
+// authenticator to check token validity & navigate 
+// based on results from API response
+
+// useeffect to configure local storage on comp mount?
+// global context for { user: , mode: }
+
 
 const router = createBrowserRouter([
   {
@@ -18,11 +28,33 @@ const router = createBrowserRouter([
   },
 ]);
 
+export const Context = createContext();
+
+
+// may need to move this so setToken is in the same scope as createContext invocation
+
+
 function App() {
+
+  const [token, setToken] = useState("");
+  const [user, setUser] = useState("charles");
+  const [mode, setMode] = useState("client");
+
+
+  const lsContent = localStorage.getItem("token");
+
+  if (token !== "") {
+    console.log("token pushed to ls");
+    localStorage.setItem("token", token);
+  } else if (token === "" && lsContent) {
+    setToken(lsContent);
+  }
 
   return (
     <>
-      <RouterProvider router={router} />
+      <Context.Provider value={{ user, mode, setToken }}>
+        <RouterProvider router={router} />
+      </Context.Provider>
     </>
   )
 }
--- client/src/endUser/homepage.jsx
+++ client/src/endUser/homepage.jsx
@@ -1,7 +1,17 @@
+import { useContext } from "react"
+import { Context } from "../App";
+
 export default function HomePage () {
+  const {user, mode} = useContext(Context);
+
   return (
     <>
       Homepage
+
+      <div>
+        user: {user}
+        mode: {mode}
+      </div>
     </>
   )
 
