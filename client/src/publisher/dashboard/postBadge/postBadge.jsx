d718c5017116270db9d9546ae0f94532715e39e6
Charles Villalpando <charlesvill@g.ucla.edu>
2025-07-14 18:04:36 -0700

    Toggle publish of post

--- client/src/publisher/dashboard/postBadge/postBadge.jsx
+++ client/src/publisher/dashboard/postBadge/postBadge.jsx
@@ -1,26 +1,57 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import styles from './postBadge.module.css'
 import { LinkButton } from '../../../utilities/linkButton';
 import { apiFetch, serverHostName } from '../../../utilities/apiUtils';
 import { Authorization } from '../../../utilities/authProvider';
+import { confirmDialog } from '../../../utilities/confirm/confirm.jsx';
 
-
-export const PostBadge = ({ post }) => {
+export const PostBadge = ({ post, dialogOpen, setDialogOpen }) => {
   const { token } = useContext(Authorization);
   const textLength = 150;
   const condensedText = post.content.slice(0, textLength);
   const [published, setPublished] = useState(post.published);
+  const visibleSVG = <svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48" onClick={toggleHandler} className={styles.projectButton}><path d="M480.118-330Q551-330 600.5-379.618q49.5-49.617 49.5-120.5Q650-571 600.382-620.5q-49.617-49.5-120.5-49.5Q409-670 359.5-620.382q-49.5 49.617-49.5 120.5Q310-429 359.618-379.5q49.617 49.5 120.5 49.5Zm-.353-58Q433-388 400.5-420.735q-32.5-32.736-32.5-79.5Q368-547 400.735-579.5q32.736-32.5 79.5-32.5Q527-612 559.5-579.265q32.5 32.736 32.5 79.5Q592-453 559.265-420.5q-32.736 32.5-79.5 32.5ZM480-200q-146 0-264-83T40-500q58-134 176-217t264-83q146 0 264 83t176 217q-58 134-176 217t-264 83Zm0-300Zm-.169 240Q601-260 702.5-325.5 804-391 857-500q-53-109-154.331-174.5-101.332-65.5-222.5-65.5Q359-740 257.5-674.5 156-609 102-500q54 109 155.331 174.5 101.332 65.5 222.5 65.5Z" /></svg>
+  const visibleOffSVG = <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000" onClick={toggleHandler} className={styles.projectButton}><path d="m644-428-58-58q9-47-27-88t-93-32l-58-58q17-8 34.5-12t37.5-4q75 0 127.5 52.5T660-500q0 20-4 37.5T644-428Zm128 126-58-56q38-29 67.5-63.5T832-500q-50-101-143.5-160.5T480-720q-29 0-57 4t-55 12l-62-62q41-17 84-25.5t90-8.5q151 0 269 83.5T920-500q-23 59-60.5 109.5T772-302Zm20 246L624-222q-35 11-70.5 16.5T480-200q-151 0-269-83.5T40-500q21-53 53-98.5t73-81.5L56-792l56-56 736 736-56 56ZM222-624q-29 26-53 57t-41 67q50 101 143.5 160.5T480-280q20 0 39-2.5t39-5.5l-36-38q-11 3-21 4.5t-21 1.5q-75 0-127.5-52.5T300-500q0-11 1.5-21t4.5-21l-84-82Zm319 93Zm-151 75Z" /></svg>
+
+  useEffect(()=>{
+    console.log(published);
+  },[published]);
 
   async function toggleHandler() {
     const publishApiUrl = `${serverHostName()}/posts/${post.id}/publish`;
     const response = await apiFetch(publishApiUrl, token, null, "PUT");
 
-    if (response instanceof Error || !response.published) {
+    if (response instanceof Error ) {
       return;
     }
 
     setPublished(response.published);
     console.log("published: ", response.published);
+  }
+
+  async function deletePost() {
+    // const publishApiUrl = `${serverHostName()}/posts/${post.id}/publish`;
+    alert("there should be a deleted item of id: ",  post.id);
+    setDialogOpen(false);
+  }
+  async function handleDelete() {
+
+    // show a dialoge box to confirm delete. 
+    if(dialogOpen){
+      return;
+    }
+
+    setDialogOpen(true);
+
+    console.log("something should really be happening rn");
+    const result = await confirmDialog({ message: "Are you sure you would like to delete?"});
+
+    if(result) {
+      deletePost();
+    }
+
+
+
   }
 
   return (
@@ -31,9 +62,9 @@ export const PostBadge = ({ post }) => {
           <p >{condensedText}</p>
         </div>
         <div className={styles.projectButtonCont}>
-          <svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="m323-205 157-94 157 95-42-178 138-120-182-16-71-168-71 167-182 16 138 120-42 178ZM233-80l65-281L80-550l288-25 112-265 112 265 288 25-218 189 65 281-247-149L233-80Zm247-355Z" /></svg>
-          <svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="M480.118-330Q551-330 600.5-379.618q49.5-49.617 49.5-120.5Q650-571 600.382-620.5q-49.617-49.5-120.5-49.5Q409-670 359.5-620.382q-49.5 49.617-49.5 120.5Q310-429 359.618-379.5q49.617 49.5 120.5 49.5Zm-.353-58Q433-388 400.5-420.735q-32.5-32.736-32.5-79.5Q368-547 400.735-579.5q32.736-32.5 79.5-32.5Q527-612 559.5-579.265q32.5 32.736 32.5 79.5Q592-453 559.265-420.5q-32.736 32.5-79.5 32.5ZM480-200q-146 0-264-83T40-500q58-134 176-217t264-83q146 0 264 83t176 217q-58 134-176 217t-264 83Zm0-300Zm-.169 240Q601-260 702.5-325.5 804-391 857-500q-53-109-154.331-174.5-101.332-65.5-222.5-65.5Q359-740 257.5-674.5 156-609 102-500q54 109 155.331 174.5 101.332 65.5 222.5 65.5Z" onClick={toggleHandler} className={styles.projectButton} /></svg>
-          <svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="M727-80q-47.5 0-80.75-33.346Q613-146.693 613-194.331q0-6.669 1.5-16.312T619-228L316-404q-15 17-37 27.5T234-366q-47.5 0-80.75-33.25T120-480q0-47.5 33.25-80.75T234-594q23 0 44 9t38 26l303-174q-3-7.071-4.5-15.911Q613-757.75 613-766q0-47.5 33.25-80.75T727-880q47.5 0 80.75 33.25T841-766q0 47.5-33.25 80.75T727-652q-23.354 0-44.677-7.5T646-684L343-516q2 8 3.5 18.5t1.5 17.741q0 7.242-1.5 15Q345-457 343-449l303 172q15-14 35-22.5t46-8.5q47.5 0 80.75 33.25T841-194q0 47.5-33.25 80.75T727-80Zm.035-632Q750-712 765.5-727.535q15.5-15.535 15.5-38.5T765.465-804.5q-15.535-15.5-38.5-15.5T688.5-804.465q-15.5 15.535-15.5 38.5t15.535 38.465q15.535 15.5 38.5 15.5Zm-493 286Q257-426 272.5-441.535q15.5-15.535 15.5-38.5T272.465-518.5q-15.535-15.5-38.5-15.5T195.5-518.465q-15.5 15.535-15.5 38.5t15.535 38.465q15.535 15.5 38.5 15.5Zm493 286Q750-140 765.5-155.535q15.5-15.535 15.5-38.5T765.465-232.5q-15.535-15.5-38.5-15.5T688.5-232.465q-15.5 15.535-15.5 38.5t15.535 38.465q15.535 15.5 38.5 15.5ZM727-766ZM234-480Zm493 286Z" /></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000" className={styles.projectButton}><path d="M240-400h320v-80H240v80Zm0-120h480v-80H240v80Zm0-120h480v-80H240v80ZM80-80v-720q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H240L80-80Zm126-240h594v-480H160v525l46-45Zm-46 0v-480 480Z" /></svg>
+          {published ? visibleSVG : visibleOffSVG}
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000" className={styles.projectButton} onClick={handleDelete}><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
         </div>
       </div>
     </div>
--- client/src/publisher/dashboard/postCollection.jsx
+++ client/src/publisher/dashboard/postCollection.jsx
@@ -1,12 +1,20 @@
 import { useOutletContext } from "react-router-dom";
+import { useState } from "react";
 import { PostBadge } from "./postBadge/postBadge.jsx"
 
 export function PostCollection() {
   const { posts } = useOutletContext();
+  const [dialogOpen, setDialogOpen] = useState(false);
+
   return (
     <div>
       <div>All Posts</div>
-      {posts && posts.map(post => <PostBadge post={post} key={post.id} />)}
+      {posts && posts.map(post => <PostBadge
+        post={post}
+        dialogOpen={dialogOpen}
+        setDialogOpen={setDialogOpen}
+        key={post.id}
+      />)}
     </div>
   )
 }
