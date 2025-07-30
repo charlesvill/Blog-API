0ddd0e74438b4205640658c93a1f9e38cd7a4b43
Charles Villalpado <charlesvill@g.ucla.edu>
2025-07-29 19:00:52 -0700

    Git restore to test vite deployment issues

--- client/src/endUser/header/clientHeader.jsx
+++ client/src/endUser/header/clientHeader.jsx
@@ -1,6 +1,7 @@
-import { useContext } from "react"
+import React, { useContext } from "react"
 import { Authorization } from "../../utilities/authProvider"
 import { LinkButton } from "../../utilities/linkButton";
+import { UserNav } from "./userNav/userNav";
 
 export function Header() {
   const { user } = useContext(Authorization);
@@ -8,7 +9,7 @@ export function Header() {
   return (
     <div>
       this is the header for client
-      {!user ? <LinkButton url={'/login'} text={"Log In"} /> : (<div>{user.first_name} <LinkButton url={"/logout"} text={"logout"}/></div>)}
+      {user && <UserNav name={user.first_name} />}
     </div>
   )
 }
---
+++ client/src/endUser/header/userNav/userNav.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { LinkButton } from "../../../utilities/linkButton";
+import styles from "./userNav.module.css";
+
+export function UserNav({ name }) {
+  const [modalActive, setModal] = useState(false);
+
+  function handleToggle(e){
+    e.preventDefault();
+
+    if(modalActive === true){
+      setModal(false);
+    } else {
+      setModal(true);
+    }
+  }
+
+  return (
+    <nav>
+      <h3 onClick={handleToggle} className={styles.userName}>{name}</h3>
+      {modalActive && (
+        <div>
+          <LinkButton url={"/admin"} text={"Author's Dashboard"} />
+          <LinkButton url={"/logout"} text={"Logout"} />
+        </div>
+      )}
+    </nav>
+  );
+}
