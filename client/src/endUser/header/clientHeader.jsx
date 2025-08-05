6b731b0e35a269e8a7963c60ebb35ba067512a28
Charles Villalpando <charlesvill@g.ucla.edu>
2025-08-04 21:08:26 -0700

    Header styling

--- client/src/endUser/header/clientHeader.jsx
+++ client/src/endUser/header/clientHeader.jsx
@@ -2,16 +2,15 @@ import React, { useContext } from "react";
 import { Authorization } from "../../utilities/authProvider";
 import { LinkButton } from "../../utilities/linkButton";
 import { UserNav } from "./userNav/userNav";
+import styles from "./clientHeader.module.css";
 
 export function Header() {
   const { user } = useContext(Authorization);
 
   return (
     <header>
-      <div>Hello world</div>
-      <div>empty div for now</div>
       <h1>BLOG</h1>
-      <div>
+      <div className={styles.userCont}>
         {!user ? (
           <LinkButton url={"/login"} text={"Log In"} />
         ) : (
