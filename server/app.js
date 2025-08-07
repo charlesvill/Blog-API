dd26d53ea78fe226cb2905aa88515f05fdf4896f
Charles Villalpando <charlesvill@g.ucla.edu>
2025-08-07 12:32:54 -0700

    edit env port variable name

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 
-const PORT = process.env.SERVER_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 const path = require("path");
 const passport = require("./authentication/passport-config.js");
 const { NotFoundError } = require("./utils/err.js");
